008b041c90c103c1ebb3038307cf26bd23710dbd
sadjei78 <sadjei78@users.noreply.github.com>
2025-04-27 03:13:41 +0000

    Deploy with updated Firebase configuration 4bc8e20dd7a4ff2f2fe0c2ba82b5841c5a52000a

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-  scriptURL: "https://script.google.com/macros/s/AKfycbxCm7yhigTmoQg3l91xhtxB3LSaOAy2lKVFWX1Ui5Ryb8iVjC0EyUjQJ9s13u6MtRQS/exec",
+  scriptURL: "https://script.google.com/macros/s/AKfycbw6NTzx1J1qJh4DzQxMfbfNamw8SN1a24ZVAuK-rBeqJlrmsOhGJGTO7nVetMVb_9lK/exec",
   githubPages: {
     baseUrl: "https://sadjei78.github.io/agl-auc25/auction.html",
     environment: "production"
